04a7b0fcaac85c847cd562551c1c2c9417242857
Bhuvan Molparia <bhuvan.molparia@gmail.com>
2016-12-08 19:17:35 -0800

    makes plot more generic

--- multi_level_pi.py
+++ multi_level_pi.py
@@ -20,7 +20,7 @@ def get_next_level_of_nodes(nodes):
 			pass
 	return new_nodes
 
-def plot(data,outname,depth_start=1,maxdepth=3):
+def plot(data,outname,depth=1,maxdepth=3,**_):
 	''' This function will plot a multi level pi chart for the given "data" in a
 	tree format. "depth" is the starting level for the pi chart, "maxdepth" is
 	the level at which the outermost layer is plotted. "outname" is the filepath
@@ -44,15 +44,15 @@ def plot(data,outname,depth_start=1,maxdepth=3):
 		colormaps = {1:'winter',2:'spectral_r',3:'Set1'}
 
 		#Get the nodes at "depth" starting from the root
-		depth = depth_start
-		current_nodes = sample.get_nodes_by_depth(depth)
+		current_depth = depth
+		current_nodes = sample.get_nodes_by_depth(current_depth)
 
-		while depth <= maxdepth:
-
+		while current_depth <= maxdepth:
+			
 			counts = [x.count for x in current_nodes]
 			labels = [x.name for x in current_nodes]
 
-			cMap      = plt.get_cmap(colormaps[depth])
+			cMap      = plt.get_cmap(colormaps[current_depth])
 			cNorm     = colors.Normalize(vmin=0, vmax=len(counts))
 			scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cMap)
 
@@ -65,19 +65,20 @@ def plot(data,outname,depth_start=1,maxdepth=3):
 			countsArr = np.array(counts)/total
 			# Width of the "rings" (percentages if the largest "radius"==1)
 			width = 0.2
-			radi  = 1 - ((maxdepth-depth)*0.3)
+			radi  = 1 - ((maxdepth-current_depth)*0.3)
 
 			kwargs = dict(startangle=90, colors=colorVals,
 						  wedgeprops={'alpha':0.8})
 			Pie, text = ax.pie(countsArr, radius = radi, **kwargs)
 
 			legend_entry = fig.legend(Pie,labels ,
-			bbox_to_anchor=((15.0/23.0)+((depth-1)*0.15), 1), loc='upper right')
+			bbox_to_anchor=((15.0/23.0)+((current_depth-1)*0.15), 1),
+							loc='upper right')
 
 			ax.add_artist(legend_entry)
 			plt.setp(Pie,width=width)
 
-			depth += 1
+			current_depth += 1
 			current_nodes = get_next_level_of_nodes(current_nodes)
 
 		pdfOut.savefig(fig)
